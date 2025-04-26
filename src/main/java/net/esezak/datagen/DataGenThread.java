5dc7a3839e5ccf0afc56841b8ceaeeaf7088ed7d
Hüseyin Yontar <120130524+HuseyinYontar@users.noreply.github.com>
2025-04-26 23:29:18 +0300

    Data simulation

--- src/main/java/net/esezak/datagen/DataGenThread.java
+++ src/main/java/net/esezak/datagen/DataGenThread.java
@@ -1,34 +1,63 @@
 package net.esezak.datagen;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class DataGenThread implements Runnable {
+    private static double cumulativeUsage = 0d;
     private static ArrayList<UsageData> usageData = new ArrayList<>();
     private boolean isDone = false;
+
+    public static void main(String[] args) {
+        DataGenThread t = new DataGenThread();
+        Thread thread = new Thread(t);
+        try{
+            thread.start();
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void run() {
         Random rd = new Random(System.currentTimeMillis());
-        int hour = 0;
-        int day = 27;
+        int hour = 1;
+        int day = 1;
         int month = 4;
         int year = 2025;
+
+        double usageHourly = 0.57869d;
+
         while (!isDone) {
             try {
-                Thread.sleep(3);
+                Thread.sleep(5);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            usageData.add(new UsageData(rd.nextDouble(0, 0.121), day, hour, month, year, rd.nextInt(0, 1)));
+
+            double usageRandom = rd.nextDouble(0.04629, 0.115738);
+            int dice = rd.nextInt(0, 2);
+            if (dice == 0) {
+                usageHourly += usageRandom;
+            } else {
+                usageHourly -= usageRandom;
+            }
+            cumulativeUsage += usageHourly;
+            usageData.add(new UsageData(cumulativeUsage, day, hour, month, year));
+
             hour++;
-            if (hour == 24) {
+            if (hour == 25) {
                 hour = 0;
                 day++;
                 if (day == 31) {
                     day = 1;
                     month++;
+                    cumulativeUsage = 0;
                     if (month == 13) {
                         month = 1;
                         year++;
@@ -38,29 +67,102 @@ public class DataGenThread implements Runnable {
         }
     }
 
-    public void stopThread(){
+    public void stopThread() {
         isDone = true;
     }
 
-    public static ArrayList<UsageData> getUsageData() {
-        //ObjectMapper mapper = new ObjectMapper();
-        //String json = mapper.writeValueAsString();
-        return usageData;
+    public String getUsageData() {
+        ObjectMapper mapper = new ObjectMapper();
+        String json;
+        try {
+            json = mapper.writeValueAsString(usageData);
+            System.out.println(json);
+            System.out.println("size:::"+ usageData.size());
+            return json;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+//    public SimpleRegression getRegressionResult(){
+//        String jsonArrayString = getUsageData();
+//        SimpleRegression regression = new SimpleRegression();
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            List<UsageData> usageDataList = mapper.readValue(jsonArrayString, new TypeReference<>() {});
+//            if (!usageDataList.isEmpty()) {
+//                int month = usageDataList.getFirst().getMonth();
+//                for (UsageData u : usageDataList) {
+//                    if (u.getHour() == 24 && u.getMonth() == month && u.getDay() < 20) {
+//                        System.out.println(u.getCumulativeUsage());
+//                        regression.addData(u.getDay(), u.getCumulativeUsage());
+//                        System.out.println(u.getDay());
+//                    }
+//                }
+//            }
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println("aaaaaaa::::"+regression.predict(30));
+//        return null;
+//    }
+
 }
-class UsageData{
-    private double usage = 0.57869d;
+
+class UsageData {
+    private double cumulativeUsage;
     private int hour;
     private int day;
     private int month;
     private int year;
 
-    public UsageData(double usage, int day, int hour, int month, int year, int dice) {
-        if(dice==0)this.usage = this.usage + usage;
-        else this.usage = this.usage - usage;
+    public UsageData(){}
+
+    public UsageData(double cumulativeUsage, int day, int hour, int month, int year) {
+        this.cumulativeUsage = cumulativeUsage;
         this.hour = hour;
         this.day = day;
         this.month = month;
+        this.year = year;
+    }
+
+    public double getCumulativeUsage() {
+        return cumulativeUsage;
+    }
+
+    public void setCumulativeUsage(double cumulativeUsage) {
+        this.cumulativeUsage = cumulativeUsage;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
         this.year = year;
     }
 }
