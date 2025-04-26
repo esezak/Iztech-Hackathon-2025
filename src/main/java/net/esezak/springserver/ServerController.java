75d7cbf91f0f6b295fd017744b82b8c8e040979d
defneeyilmaz <defneyilmaz210@gmail.com>
2025-04-27 02:47:39 +0300

    added webapp

--- src/main/java/net/esezak/springserver/ServerController.java
+++ src/main/java/net/esezak/springserver/ServerController.java
@@ -1,16 +1,20 @@
 package net.esezak.springserver;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class ServerController {
 
     @GetMapping("/getdata")
     public String getData() {
         return "Hello World!";
+    }
+
+    public String index() {
+        return "index.html";
     }
 
     @GetMapping("/askai")
